166caea9d2c79ce69d3299227f302ae2f821fcc7
segoleneimie <45560352+segoleneimie@users.noreply.github.com>
2018-12-09 17:42:19 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+    	<img src="images/logo/barrelogo2.png"/>
+        <img src="images/logo/gf.ico" alt="logo"/>
+        <p class="titre">Goodfood</p>
+        <p class="soustitre">Good mood</p>
+<!-- Nav sur toutes les pages -->
+        <nav> 
+            <a id="plats" href="html/plats.html">Nos plats</a>
+            <a id="activite" href="html/activite.html">Nos activités</a>
+            <a id="actu" href="html/actu.html">Actualités</a>
+            <a id="contact" href="html/reservation.html">Contacts / Réservation</a>
+            <a id="equipe" href="html/notreEquipe.html">Notre équipe / recrutement</a>
+            <a id="galerie" href="html/galerie.html">Notre équipe / recrutement</a>
+        </nav>
+    </header>
